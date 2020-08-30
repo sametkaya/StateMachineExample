6404e236b62108c466910cf09c4416f35a3a2208
samet kaya <sametky@outlook.com>
2020-08-30 14:34:12 +0300

    first load

---
+++ StateMachine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            /*
+            Console.WriteLine(TheMachine.CheckIt("aab"));
+            Console.WriteLine(TheMachine.CheckIt("aabb"));
+            Console.WriteLine(TheMachine.CheckIt("aa"));
+            Console.WriteLine(TheMachine.CheckIt("aabaabb"));
+            Console.WriteLine(TheMachine.CheckIt(""));
+            Console.WriteLine(TheMachine.CheckIt("aaa"));*/
+            string exit = "Y";
+            Console.WriteLine("((a+b)*ab+(aa)*(bb)*)*");
+            while (exit == "Y")
+            {
+                Console.WriteLine("Type A Word With Alphabet {a,b} : ");
+                string word=Console.ReadLine();
+                Console.WriteLine(TheMachine.CheckIt(word));
+
+                Console.WriteLine("Do You Want To Exit Y/N");
+                exit=Console.ReadLine();
+
+            }
+            
+
+
+            Console.ReadKey();
+        }
+
+
+
+    }
+}
---
+++ StateMachine/TheMachine.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachine
+{
+    
+    class TheMachine
+    {
+        public enum States {A,B,C,D}
+        public static char[] Alphabet = new char[] {'a','b' };
+        public static States[] FiniteStates = new States[] { States.A, States.D };
+        public static  bool CheckIt(string word)
+        {
+
+            Console.Write(word + " is ");
+            bool isAccepted = false;
+            States state = States.A; //initial contition
+
+            foreach (char c in word)
+            {
+
+                if (!Alphabet.Contains(c))
+                {
+                    Console.WriteLine(c+ " Not In The Alphabet");
+                    isAccepted = false;
+                    return isAccepted;
+                }
+                switch (c)
+                {
+                    case 'a':
+                        switch (state)
+                        {
+                            case States.A:
+                                state = States.B;
+                                break;
+                            case States.B:
+                                state = States.D;
+                                break;
+                            case States.C:
+                                state = States.C;
+                                break;
+                            case States.D:
+                                state = States.B;
+                                break;
+                       
+                        }
+                        break;
+                    case 'b':
+                        switch (state)
+                        {
+                            case States.A:
+                                state = States.C;
+                                break;
+                            case States.B:
+                                state = States.A;
+                                break;
+                            case States.C:
+                                state = States.A;
+                                break;
+                            case States.D:
+                                state = States.D;
+                                break;
+                            default:
+                                break;
+                        }
+                        break; 
+                }
+            }
+
+            if (FiniteStates.Contains(state))
+            {
+                isAccepted = true;
+            }
+            else
+            {
+                isAccepted = false;
+            }
+
+            return isAccepted;
+
+        }
+
+    }
+}
